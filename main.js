6e15f5cfca31a8cab79b7eafc6201f46824f2536
Maxim Goryachev <maxto4kacom@gmail.com>
2017-01-30 22:54:21 +0200

    test

---
+++ main.js
@@ -0,0 +1,21 @@
+var table = $('#exampl').DataTable();
+
+$('#btn-1').on( 'click', function () {
+    table.page.len(3).draw();
+} );
+
+$('#btn-2').on( 'click', function () {
+    table.page.len(6).draw();
+} );
+
+$('#btn-3').on( 'click', function () {
+    table.page.len(9).draw();
+} );
+
+$('#btn-4').on( 'click', function () {
+    table.page.len(12).draw();
+} );
+
+$('#btn-5').on( 'click', function () {
+    table.page.len(15).draw();
+} );
